5c76734f39dcd2401f66ed77f10946451473e106
Lucas Melo <luluco250@gmail.com>
2020-06-15 11:54:07 -0300

    First commit.

---
+++ readme.md
@@ -0,0 +1,12 @@
+# Discord Web Bing Background
+
+User script and style for using Bing's daily wallpapers as Discord web's
+background.
+
+
+To use, simply import `userscript.js` into Greasemonkey/Tampermonkey/equivalent
+and `userstyle.css` into Stylus/Stylish.
+
+
+There are some elements of the UI that are transparent where they shouldn't be,
+such as the emoji picker, but it's minor enough not to be a problem.
---
+++ userscript.js
@@ -0,0 +1,47 @@
+// ==UserScript==
+// @name     Discord Bing Background
+// @version  1
+// @include https://discord.com/channels/*
+// ==/UserScript==
+
+window.addEventListener("load", () => {
+	"use strict";
+
+	function myLog(...args) {
+	  console.log(
+		"[MESSG] Bing Wallpaper Changer:\n",
+		args.reduce((prev, curr) => `${prev} ${curr}`));
+	}
+
+	function myError(...args) {
+	  console.error(
+		"[ERROR] Bing Wallpaper Changer:\n",
+		  args.reduce((prev, curr) => `${prev} ${curr}`));
+	}
+
+	try {
+	  const bingUrl = "http://www.bing.com/HPImageArchive.aspx?format=xml&idx=0&n=1&mkt=en-US";
+
+	  const req = new XMLHttpRequest();
+	  req.open("GET", bingUrl, false);
+	  req.send();
+
+	  const bingXml = req.responseXML;
+	  const urlObj = bingXml.getElementsByTagName("url")[0];
+	  const imgUrl = `https://bing.com${urlObj.textContent}`;
+
+	  req.open("GET", imgUrl, false);
+	  req.responseType = "arraybuffer";
+	  req.send();
+
+	  const imgBlob = new Blob([req.response], {type: "image/jpeg"});
+	  const blobUrl = URL.createObjectURL(imgBlob);
+
+	  const img = document.createElement("img");
+	  img.id = "my-background";
+	  img.src = blobUrl;
+	  document.body.prepend(img);
+	} catch (e) {
+	  myError(e);
+	}
+  })();
