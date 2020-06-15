3f7574e118cfa68376f03497ad1d9113eae62acd
Lucas Melo <luluco250@gmail.com>
2020-06-15 11:56:02 -0300

    Fixed indentation in userscript.

--- userscript.js
+++ userscript.js
@@ -8,40 +8,40 @@ window.addEventListener("load", () => {
 	"use strict";
 
 	function myLog(...args) {
-	  console.log(
-		"[MESSG] Bing Wallpaper Changer:\n",
-		args.reduce((prev, curr) => `${prev} ${curr}`));
+		console.log(
+			"[MESSG] Bing Wallpaper Changer:\n",
+			args.reduce((prev, curr) => `${prev} ${curr}`));
 	}
 
 	function myError(...args) {
-	  console.error(
-		"[ERROR] Bing Wallpaper Changer:\n",
-		  args.reduce((prev, curr) => `${prev} ${curr}`));
+		console.error(
+			"[ERROR] Bing Wallpaper Changer:\n",
+			args.reduce((prev, curr) => `${prev} ${curr}`));
 	}
 
 	try {
-	  const bingUrl = "http://www.bing.com/HPImageArchive.aspx?format=xml&idx=0&n=1&mkt=en-US";
+		const bingUrl = "http://www.bing.com/HPImageArchive.aspx?format=xml&idx=0&n=1&mkt=en-US";
 
-	  const req = new XMLHttpRequest();
-	  req.open("GET", bingUrl, false);
-	  req.send();
+		const req = new XMLHttpRequest();
+		req.open("GET", bingUrl, false);
+		req.send();
 
-	  const bingXml = req.responseXML;
-	  const urlObj = bingXml.getElementsByTagName("url")[0];
-	  const imgUrl = `https://bing.com${urlObj.textContent}`;
+		const bingXml = req.responseXML;
+		const urlObj = bingXml.getElementsByTagName("url")[0];
+		const imgUrl = `https://bing.com${urlObj.textContent}`;
 
-	  req.open("GET", imgUrl, false);
-	  req.responseType = "arraybuffer";
-	  req.send();
+		req.open("GET", imgUrl, false);
+		req.responseType = "arraybuffer";
+		req.send();
 
-	  const imgBlob = new Blob([req.response], {type: "image/jpeg"});
-	  const blobUrl = URL.createObjectURL(imgBlob);
+		const imgBlob = new Blob([req.response], {type: "image/jpeg"});
+		const blobUrl = URL.createObjectURL(imgBlob);
 
-	  const img = document.createElement("img");
-	  img.id = "my-background";
-	  img.src = blobUrl;
-	  document.body.prepend(img);
+		const img = document.createElement("img");
+		img.id = "my-background";
+		img.src = blobUrl;
+		document.body.prepend(img);
 	} catch (e) {
-	  myError(e);
+		myError(e);
 	}
-  })();
+})();
